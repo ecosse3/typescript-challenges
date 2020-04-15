844982add382925af39635cdec2d6f53a62f8f56
ecosse3 <theecosse@gmail.com>
2020-04-15 21:16:53 +0200

    Only one pass

--- src/only-one/only-one.ts
+++ src/only-one/only-one.ts
@@ -1,42 +1,45 @@
 /*
-* Przeprogramowani.ts - https://przeprogramowani.pl/typescript/
-*
-* Only one
-* ------------------
-*
-* Goal: Make sure that both `eatBanana` and `eatMouse` can be called inside `eatSomething`.
-* 
-* Hint: https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-guards-and-differentiating-types
-*/
+ * Przeprogramowani.ts - https://przeprogramowani.pl/typescript/
+ *
+ * Only one
+ * ------------------
+ *
+ * Goal: Make sure that both `eatBanana` and `eatMouse` can be called inside `eatSomething`.
+ *
+ * Hint: https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-guards-and-differentiating-types
+ */
 
-class Monkey {
-    eatBanana() {
-        return 'Eating banana!'
+interface IEat {
+    eat?: () => string;
+}
+
+class Monkey implements IEat {
+    eat() {
+        return "Eating banana!";
     }
 }
 
-class Snake {
-    eatMouse() {
-        return 'Eating mouse!'
+class Snake implements IEat {
+    eat() {
+        return "Eating mouse!";
     }
 }
 
-const monkey = new Monkey()
-const snake = new Snake()
+const monkey = new Monkey();
+const snake = new Snake();
 
-type AnimalInZoo = Monkey | Snake
+type AnimalInZoo = Monkey | Snake;
 
 function eatSomething(animal: AnimalInZoo) {
-    animal.eatBanana();
-    animal.eatMouse();
+    return animal.eat();
 }
 
 /* Do not modify tests */
 
-test('should eat banana', () => {
-    expect(eatSomething(monkey)).toBe('Eating banana!')
-})
+test("should eat banana", () => {
+    expect(eatSomething(monkey)).toBe("Eating banana!");
+});
 
-test('should eat mouse', () => {
-    expect(eatSomething(snake)).toBe('Eating mouse!')
-})
+test("should eat mouse", () => {
+    expect(eatSomething(snake)).toBe("Eating mouse!");
+});
